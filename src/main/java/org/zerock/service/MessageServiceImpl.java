655a4eeedd7af04bd7bf6a820e59675b1e8fef8b
cuteprettykara <cuteprettykara@gmail.com>
2017-04-11 11:43:28 +0900

    Part4 chapter 4. AOP와 트랜잭션 처리(Spring의 트랜잭션 처리)

--- src/main/java/org/zerock/service/MessageServiceImpl.java
+++ src/main/java/org/zerock/service/MessageServiceImpl.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.domain.MessageVO;
 import org.zerock.persistence.MessageDAO;
 import org.zerock.persistence.PointDAO;
@@ -19,6 +20,7 @@ public class MessageServiceImpl implements MessageService {
 	@Inject
 	private PointDAO pointDAO;
 	
+	@Transactional
 	@Override
 	public void addMessage(MessageVO vo) throws Exception {
 		messageDAO.create(vo);
